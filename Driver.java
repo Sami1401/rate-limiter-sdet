de79b83d782eea8cc658321ad202e64078bbbaa1
Sami1401 <98746542+Sami1401@users.noreply.github.com>
2026-03-16 12:33:03 +0530

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,1 +1,15 @@
+package ratelimiter;
 
+public class Driver {
+
+    public static void main(String[] args) {
+
+        RateLimiter limiter = new RateLimiter(3, 10);
+
+        System.out.println(limiter.allowRequest("A",1));
+        System.out.println(limiter.allowRequest("A",2));
+        System.out.println(limiter.allowRequest("A",3));
+        System.out.println(limiter.allowRequest("A",9));
+        System.out.println(limiter.allowRequest("A",12));
+    }
+}
